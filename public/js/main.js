a3f890ce10d15397bf45504bab379daed1e32e57
Jonathan Cardona <jcardona0110@gmail.com>
2023-09-10 15:36:03 -0400

    added new reqs, new data to collection

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+let hunters = [
+	{
+		"name": "Gon Freecss",
+		"age": 14,
+		"gender": "male",
+		"status": "alive",
+		"affiliation": "Hunter Association",
+		"occupation": "Rookie Hunter",
+		"nenType": "Enhancer",
+		"nenSpecial": "Jajanken",
+	},
+
+	{
+		"name": "Killua Zoldyck",
+		"age": 14,
+		"gender": "male",
+		"status": "alive",
+		"affiliation": "Hunter Association",
+		"occupation": "Rookie Hunter",
+		"nenType": "transmuter",
+		"nenSpecial": "Godspeed",
+	},
+];
+// blank template
+// {
+// 		"name": ,
+// 		"age": ,
+// 		"gender": ,
+// 		"status": ,
+// 		"affiliation": ,
+// 		"occupation": ,
+// 		"nenType": ,
+// 		"nenSpecial": ,
+// 	}
--- server.js
+++ server.js
@@ -1,55 +1,58 @@
 // hunter x hunter api where people can request data regarding the different hunters in the anime/manga's universe
 // attributes that users can request per hunter will include: name, age, gender, state(d or a) affiliation, occupation, nen type, nen abilites
-require('dotenv').config();
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
 // connect to DB
 let db;
 let dbConnectionString = process.env.DB_STRING;
-let dbName = 'Hunter';
+let dbName = "Hunter";
 
-const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(dbConnectionString).then((client) => {
 	console.log(`Connected to ${dbName} Database`);
 	db = client.db(dbName);
 });
 
 // connect to server
-let PORT;
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+let PORT = process.env.PORT;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
 	console.log(`the server is now running on port ${PORT}! Betta go catch it!`);
 });
 
-let hunters = [
-	{
-		name: 'Gon Freecss',
-		age: 14,
-		gender: 'male',
-		status: 'alive',
-		affiliation: 'Hunter Association',
-		occupation: 'Rookie Hunter',
-		nenType: 'Enhancer',
-		nenSpecial: 'Jajanken',
-	},
+app.get("/", (request, response) => {
+	db.collection("Hunters")
+		.find()
+		.toArray()
+		.then((data) => {
+			response.render("index.ejs", { info: data });
+		})
+		.catch((error) => console.error(error));
+});
 
-	{
-		name: 'Killua Zoldyck',
-		age: 14,
-		gender: 'male',
-		status: 'alive',
-		affiliation: 'Hunter Association',
-		occupation: 'Rookie Hunter',
-		nenType: 'transmuter',
-		nenSpecial: 'Godspeed',
-	},
-];
+app.post("/addHunter", (request, response) => {
+	db.collection("Hunters")
+		.insertOne(request.body.name)
+		.then((result) => {
+			console.log("Hunter Added");
+			response.redirect("/");
+		})
+		.catch((error) => console.error(error));
+});
 
-app.get('/', (req, res) => {
-	res.json(hunters);
+app.delete("/deleteHunter", (request, response) => {
+	db.collection("Hunters")
+		.deleteOne({ stageName: request.body.stageNameS })
+		.then((result) => {
+			console.log("Hunter Deleted");
+			response.json("Hunter Deleted");
+		})
+		.catch((error) => console.error(error));
 });
